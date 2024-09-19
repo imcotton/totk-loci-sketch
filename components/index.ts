83d73e1e74c58b62bf4b87b04293bc7417915974
Cotton Hou <himcotton@gmail.com>
2024-09-19 17:15:20 +0800

    add Outline

--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,5 @@ export { DraftForm } from './draft-form.tsx';
 
 export { OtpSetup } from './otp-setup.tsx';
 
+export { Outline } from './outline.tsx';
+
---
+++ components/outline.tsx
@@ -0,0 +1,87 @@
+/** @jsx jsx */ void jsx;
+
+import { jsx, memo } from 'hono/jsx';
+
+import type { Articles } from '../articles.ts';
+
+
+
+
+
+export const Outline = memo(function ({ draft, published }: Articles) {
+
+    return (<div>
+
+        { draft.length > 0 && <details>
+
+            <summary role="button" class="secondary outline">
+                <i>Draft</i>
+            </summary>
+
+            <ul>
+
+                { draft.slice(0, 3).map(item => <li>
+
+                    <a href={ item.url } target="_blank">
+                        { item.slug }
+                    </a>
+
+                    { item.title.slice(9) }
+
+                </li>) }
+
+            </ul>
+
+        </details> }
+
+        { published.length > 0 && <details open>
+
+            <summary role="button" class="outline">
+                Published
+            </summary>
+
+            <ul>
+
+                { published.slice(0, 5).map(item => <li>
+
+                    <strong>
+                        <a href={ item.url } target="_blank">
+                            { item.slug }
+                        </a>
+                    </strong>
+
+                    { item.title.slice(9) }
+
+                </li>) }
+
+            </ul>
+
+        </details> }
+
+    </div>);
+
+}, (fst, snd) => compare(fst.published, snd.published)
+              && compare(fst.draft,     snd.draft)
+);
+
+
+
+
+
+function compare <T extends Pick<Articles['draft'][number], 'slug'>> (
+
+        fst: readonly T[],
+        snd: readonly T[],
+
+): boolean {
+
+    if (fst.length === snd.length) {
+
+        return fst.every((x, i) => x.slug === snd.at(i)?.slug);
+
+    }
+
+    return false;
+
+}
+
