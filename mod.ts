5bff8db34a280188caa2d9a3c7a5cf8c21cc7a71
Cotton Hou <himcotton@gmail.com>
2024-09-26 07:03:54 +0800

    export all from mod

--- mod.ts
+++ mod.ts
@@ -1,3 +1,5 @@
 export { main, type Inputs, type Checksum } from './main.ts';
 export { make, type Outputs } from './make.ts';
+export { create_app } from './app.tsx';
+export { default } from './deploy/deno.ts';
 
