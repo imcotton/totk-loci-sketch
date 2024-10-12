b58483ab7c6bef3014fd9a9d11851f8f71ae12c3
Cotton Hou <himcotton@gmail.com>
2024-10-12 10:09:11 +0800

    component AuthKeySetup

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ export { DraftForm } from './draft-form.tsx';
 
 export { OtpSetup } from './otp-setup.tsx';
 
+export { AuthKeySetup } from './auth-key-setup.tsx';
+
 export { Outline } from './outline.tsx';
 
